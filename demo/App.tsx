825bdcde2f4effb9d30d189b030aca3a9fda2cde
keithchasen <keithchasen89@gmail.com>
2025-07-11 22:27:46 +0200

    demo react app

---
+++ demo/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'chase-ui';
+
+export const App = () => {
+	return (
+		<div>
+			<h1>UI Demo</h1>
+			<Button color='red'>test button</Button>
+		</div>
+	);
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,41 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
+import * as path from 'node:path';
 
-export default defineConfig({
-	plugins: [react(), dts({ insertTypesEntry: true })],
-	build: {
-		lib: {
-			entry: 'src/index.ts',
-			name: 'ChaseUi',
-			fileName: format => `chase-ui.${format}.js`,
-			formats: ['es', 'umd'],
-		},
-		rollupOptions: {
-			external: ['react', 'react-dom'],
-			output: {
-				globals: {
-					react: 'React',
-					'react-dom': 'ReactDOM',
-				},
+export default defineConfig(({ command }) => {
+	const isBuild = command === 'build';
+
+	return {
+		root: isBuild ? '.' : 'demo',
+		plugins: [react(), isBuild && dts({ insertTypesEntry: true })].filter(
+			Boolean
+		),
+
+		resolve: {
+			alias: {
+				'chase-ui': path.resolve(__dirname, 'src'),
 			},
 		},
-	},
+
+		build: isBuild
+			? {
+					lib: {
+						entry: 'src/index.ts',
+						name: 'ChaseUI',
+						fileName: format => `chase-ui.${format}.js`,
+						formats: ['es', 'umd'],
+					},
+					rollupOptions: {
+						external: ['react', 'react-dom'],
+						output: {
+							globals: {
+								react: 'React',
+								'react-dom': 'ReactDOM',
+							},
+						},
+					},
+				}
+			: undefined,
+	};
 });
